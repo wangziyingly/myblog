b2460157562cc80d872e1f8dcad69dcade3a6fed
wangziyingly <wangziyingly@users.noreply.github.com>
2026-07-20 15:36:41 +0800

    chore: 更新 site 为正式部署地址

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://myblog-lime-one.vercel.app/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
