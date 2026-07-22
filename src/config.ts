e639dd28b3a3c8161b8e7dcaca815255fe27abbe
wangziyingly <wangziyingly@users.noreply.github.com>
2026-07-22 10:29:11 +0800

    chore: GitHub 链接指向个人主页

--- src/config.ts
+++ src/config.ts
@@ -66,7 +66,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com",
+			url: "https://github.com/wangziyingly",
 		},
 		{
 			name: "Bilibili",
